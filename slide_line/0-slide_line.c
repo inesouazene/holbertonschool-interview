1b60ceb283cb1ae9db3a58cfd6744fa6252962de
inesouazene <inesouazene@gmail.com>
2025-02-26 16:12:52 +0100

    corrections

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,84 +1,6 @@
 #include "slide_line.h"
 
 /**
- * slide_left - Slides and merges an array of integers to the left
- * @line: Points to an array of integers
- * @size: Number of elements in the array
- * Return: 1 upon success, 0 upon failure
- */
-static int slide_left(int *line, size_t size)
-{
-	size_t i, pos = 0;
-	int merged = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		if (line[i] == 0)
-			continue;
-
-		/* If we have a number at pos and it's the same as current number */
-		if (pos > 0 && line[pos - 1] == line[i] && !merged)
-		{
-			line[pos - 1] *= 2; /* Double the value */
-			line[i] = 0;        /* Set current position to 0 */
-			merged = 1;         /* Mark as merged */
-		}
-		else
-		{
-			/* If current position isn't the same as the position to place */
-			if (i != pos)
-			{
-				line[pos] = line[i]; /* Move number to new position */
-				line[i] = 0;         /* Set current position to 0 */
-			}
-			pos++;       /* Increment position */
-			merged = 0;  /* Reset merged flag */
-		}
-	}
-
-	return (1);
-}
-
-/**
- * slide_right - Slides and merges an array of integers to the right
- * @line: Points to an array of integers
- * @size: Number of elements in the array
- * Return: 1 upon success, 0 upon failure
- */
-static int slide_right(int *line, size_t size)
-{
-	int i, pos = size - 1;
-	int merged = 0;
-
-	for (i = size - 1; i >= 0; i--)
-	{
-		if (line[i] == 0)
-			continue;
-
-		/* If we have a number at pos and it's the same as current number */
-		if (pos < size - 1 && line[pos + 1] == line[i] && !merged)
-		{
-			line[pos + 1] *= 2; /* Double the value */
-			line[i] = 0;        /* Set current position to 0 */
-			merged = 1;         /* Mark as merged */
-		}
-		else
-		{
-			/* If current position isn't the same as the position to place */
-			if (i != pos)
-			{
-				line[pos] = line[i]; /* Move number to new position */
-				line[i] = 0;         /* Set current position to 0 */
-			}
-			pos--;       /* Decrement position */
-			merged = 0;  /* Reset merged flag */
-		}
-	}
-
-	return (1);
-}
-
-/**
  * slide_line - Slides and merges an array of integers
  * @line: Points to an array of integers
  * @size: Number of elements in the array
@@ -87,6 +9,10 @@ static int slide_right(int *line, size_t size)
  */
 int slide_line(int *line, size_t size, int direction)
 {
+	size_t i, pos;
+	int merged = 0;
+
+	/* Check for invalid parameters */
 	if (line == NULL || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
 		return (0);
 
@@ -94,7 +20,59 @@ int slide_line(int *line, size_t size, int direction)
 		return (1);
 
 	if (direction == SLIDE_LEFT)
-		return (slide_left(line, size));
-	else
-		return (slide_right(line, size));
+	{
+		/* Slide to the left */
+		pos = 0;
+		for (i = 0; i < size; i++)
+		{
+			if (line[i] == 0)
+				continue;
+
+			if (pos > 0 && line[pos - 1] == line[i] && !merged)
+			{
+				line[pos - 1] *= 2;
+				line[i] = 0;
+				merged = 1;
+			}
+			else
+			{
+				if (i != pos)
+				{
+					line[pos] = line[i];
+					line[i] = 0;
+				}
+				pos++;
+				merged = 0;
+			}
+		}
+	}
+	else /* SLIDE_RIGHT */
+	{
+		/* Slide to the right */
+		pos = size - 1;
+		for (i = size; i > 0; i--)
+		{
+			if (line[i - 1] == 0)
+				continue;
+
+			if (pos < size - 1 && line[pos + 1] == line[i - 1] && !merged)
+			{
+				line[pos + 1] *= 2;
+				line[i - 1] = 0;
+				merged = 1;
+			}
+			else
+			{
+				if (i - 1 != pos)
+				{
+					line[pos] = line[i - 1];
+					line[i - 1] = 0;
+				}
+				pos--;
+				merged = 0;
+			}
+		}
+	}
+
+	return (1);
 }
