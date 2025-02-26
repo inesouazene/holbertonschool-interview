e7adda3ade7964f40faa1c5b82142ac33cf43cb8
inesouazene <inesouazene@gmail.com>
2025-02-26 15:41:24 +0100

    corrections on files

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,26 +1,6 @@
 #include <stdio.h>
 #include "sandpiles.h"
 
-/**
- * print_grid - Prints a 3x3 grid
- * @grid: 3x3 grid to be printed
- */
-void print_grid(int grid[3][3])
-{
-    int i, j;
-
-    printf("=\n");
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
-}
 
 /**
  * is_stable - Checks if a sandpile is stable
