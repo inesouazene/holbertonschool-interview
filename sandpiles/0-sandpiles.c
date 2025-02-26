c821fe98e7b4c74b0cae2249b21211581e96fc56
inesouazene <inesouazene@gmail.com>
2025-02-26 15:49:38 +0100

    new corrections

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
 #include "sandpiles.h"
 
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+ static void print_grid(int grid[3][3])
+ {
+     int i, j;
+ 
+     for (i = 0; i < 3; i++)
+     {
+         for (j = 0; j < 3; j++)
+         {
+             if (j)
+                 printf(" ");
+             printf("%d", grid[i][j]);
+         }
+         printf("\n");
+     }
+ }
 
 /**
  * is_stable - Checks if a sandpile is stable
--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -2,8 +2,8 @@
 #define SANDPILES_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-int is_stable(int grid[3][3]);
 
 #endif /* SANDPILES_H */
